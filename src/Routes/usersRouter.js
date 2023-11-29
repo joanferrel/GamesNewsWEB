0737b28ba66b9d88c7c6a1526f9c9ac352853740
Joan Ferrel <ferrel007_2000@hotmail.com>
2023-11-29 16:56:13 -0500

    handlers de usuario funciona

--- src/Routes/usersRouter.js
+++ src/Routes/usersRouter.js
@@ -1,5 +1,5 @@
 const {Router} = require ("express");
-const usersRouter = Router();
+
 
 //Error1
 //TIenes dos errores:
@@ -10,12 +10,24 @@ const usersRouter = Router();
 //}); 
 
 //La solucion esta como lo ves aca abajo:
-usersRouter.get("/", (req, res)=>{ //RUTA
-    res.status(200).send("Aqui estan todos lo usuarios"); //HANDLER
-});
+// usersRouter.get("/", (req, res)=>{ //RUTA
+//     res.status(200).send("Aqui estan todos lo usuarios"); //HANDLER
+// });
 
-usersRouter.get("/:id", (req, res)=>{ //RUTA
-    res.status(200).send("detalle del usuario"); //HANDLER
-});
-
+// usersRouter.get("/:id", (req, res)=>{ //RUTA
+//     res.status(200).send("detalle del usuario"); //HANDLER
+// });
+// Modularizado para el uso de handlers::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const {getDetailHandler,getUsersHandler}= require("../handlers/userHandler");
+const usersRouter = Router();
+//IMPORTANTE
+//Los hadlerseran convocados cuando lleguen al /"" o al /:id
+//En los comentarios de arriba son la forma NO modularizada les creamos
+// un espacio donde estaran los handlers osea acciones de lo que sucedera en es ruta
+//solo ve a userHandler ahi esta lo mismo de arriba solo que en otrolugar
+//Handler s eencarga de recibir la solicitud,unificar datomdevolver repeusta
+//E activa a controller PERO NUNCA INTERACTUAN CON LA BASE DE DATOS O IP eso lo ara el controller
+// El controller e otra funcion
+usersRouter.get("/", getUsersHandler);
+usersRouter.get("/:id", getDetailHandler);
 module.exports = usersRouter;
--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -1,5 +1,11 @@
-usersRouter.get("/users", (req, res)=>{ //RUTA
+const getUsersHandler= (req, res)=>{ //RUTA
     res.status(200).send("Aqui estan todos lo usuarios"); //HANDLER
-});
+};
 
-module.exports = usersRouter
+const getDetailHandler= (req, res)=>{ //RUTA
+    res.status(200).send("Detalle del usuario"); //HANDLER
+};
+module.exports = {
+    getDetailHandler,
+    getUsersHandler,
+}
