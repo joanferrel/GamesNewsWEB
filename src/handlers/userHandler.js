03a023638ef083136a3d1b827bed7d0efb41db3c
Joan Ferrel <ferrel007_2000@hotmail.com>
2023-12-06 11:35:20 -0500

    controlador de creacion echo

--- src/handlers/userHandler.js
+++ src/handlers/userHandler.js
@@ -1,10 +1,10 @@
 //GET
+const {createUserDB} = require("../controlers/userControllers")
 const getUsersHandler= (req, res)=>{ //RUTA
 
 
     const {name,race}= req.query;
     if(name) res.status(200).send(`Aqui esta el usuario ${name}`);
-    res.status(200).send(`Todos los usuarios`);
 
 
     res.status(200).send("Aqui estan todos lo usuarios"); //HANDLER
@@ -19,9 +19,16 @@ const getDetailHandler= (req, res)=>{ //RUTA
 //Prueba lo anterior fue para ver aora este ps par acrear pero en forma deeejmplo
 //POST
 //Body puede venr por informacion de formualrio no te olvides del parseo a app.js
-const createUserHandler= (req, res)=>{ //RUTA
+const createUserHandler= async(req, res)=>{ //RUTA
     const{name,email,phone}= req.body;
-    res.status(200).send(`Usuario ${name} creando con el email ${email}`); //HANDLER
+    try {
+        const response = await createUserDB(name,email,phone)
+        res.status(200).json(response)
+        return;
+    } catch (error) {
+        res.status(400).json({error:error.message})
+        
+    }
 };
 
 module.exports = {
